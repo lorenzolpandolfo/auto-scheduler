59388b03fa95587baab188e7db0820466287d3e0
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-11-22 10:14:47 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Checar no Google Agenda horários disponíveis dentre um intervalo de tempo
+Criar uma imagem .png organizada informando os horários disponíveis para agendamento.
+A imagem será utilizada nas redes sociais do estabelecimento.
+"""
