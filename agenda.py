4c6f6f20a13c1c99ee717dd6709fa0568988cd74
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-11-22 11:58:42 -0300

    development

--- agenda.py
+++ agenda.py
@@ -1,1 +1,27 @@
 import datetime
+
+def checar_horario(eventos):
+    todos_horarios = []
+
+    for event in eventos:
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        
+        # Converter a string do horário para um objeto datetime
+        start_datetime = datetime.datetime.fromisoformat(start)
+        
+        """
+        # Definir o horário desejado para comparação
+        horario_desejado = datetime.time(8, 00)
+        """
+        
+        horario = start_datetime.time()
+        todos_horarios.append(horario)
+        
+        """
+        if start_datetime.time() == horario_desejado:
+            print("Certo")
+        else:
+            print(start, event["summary"])
+        """
+
+    return todos_horarios    
--- main.py
+++ main.py
@@ -3,16 +3,14 @@ Checar no Google Agenda horários disponíveis dentre um intervalo de tempo
 Criar uma imagem .png organizada informando os horários disponíveis para agendamento.
 A imagem será utilizada nas redes sociais do estabelecimento.
 """
-from PIL import Image, ImageDraw, ImageFont
 import image
 import day
 import quickstart
+import agenda
 
 if __name__ == "__main__":
     eventos = quickstart.main()
-    for event in eventos:
-        start = event["start"].get("dateTime", event["start"].get("date"))
-        print(start, event["summary"])
+    horarios_agendados = agenda.horarios_agendados(eventos)
 
     dia = day.gerar_dia_da_semana()
     day.escrever_dia(image.draw, dia)
