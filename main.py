dbbaedf453788fd89684ce539d80dec9e9af1892
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-11-22 10:23:19 -0300

    test ssh key

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ Checar no Google Agenda horários disponíveis dentre um intervalo de tempo
 Criar uma imagem .png organizada informando os horários disponíveis para agendamento.
 A imagem será utilizada nas redes sociais do estabelecimento.
 """
+
