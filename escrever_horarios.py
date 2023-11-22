59a4b93b8d7fec0056e615b9c069411ceb0878ba
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-11-22 16:20:28 -0300

    base pronta

--- escrever_horarios.py
+++ escrever_horarios.py
@@ -8,19 +8,17 @@ from datetime import datetime
 def escrever_horarios_disponiveis(horarios, draw):
     text_color = (0,0,0)
     fonte = ImageFont.load_default(size=75)
-    text_x = 0
-    text_y = 100
+    text_y = 420
+    text_x = 120
     
-    for horario, i in enumerate(horarios):
-        # escrever na imagem o horario
-        # print(i, horario)
-        
-        if horario % 3 == 0:
-            text_y += 200
-            text_x = 0
-        
-        text_y += 200
-        
+    for i, horario in enumerate(horarios):
+        if i % 3 == 0:
+            text_x = 120
+            text_y += 175
+        else:
+            text_x += 300
 
         text_pos = (text_x, text_y)
-        draw.text(text_pos, str(i), font=fonte, fill=text_color)
+        draw.text(text_pos, str(horario), font=fonte, fill=text_color)
+
+    return 0
